493c675fc5b9a4e3dc5fd5430720d847a60521aa
Elliot Trapp <elliot@trapp.codes>
2023-11-17 10:09:59 -0800

    cleaning up styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
     'postcss-inline-svg': {
       paths: ['src/assets/images/icons'],
     },
